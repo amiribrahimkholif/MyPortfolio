2d7803164cf2a77bf03cd2c4725fe6213fcb38af
amiribrahimfkholif <amiribrahimfkholif@gmail.com>
2021-03-26 18:05:41 +0200

    last commit

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork<Owner> _owner;
+        private readonly IUnitOfWork<PortfolioItem> _portfolio;
+        public HomeController(IUnitOfWork<Owner>owner,
+            IUnitOfWork<PortfolioItem>portfolio)
+        {
+            _owner = owner;
+            _portfolio = portfolio;
+        }
+
+        public IUnitOfWork<PortfolioItem> Portfolio { get; }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                Owner = _owner.Entity.GetAll().First(),
+                PortfolioItems = _portfolio.Entity.GetAll().ToList()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
